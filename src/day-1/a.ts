fc26bc30ed21ab0caff588a943d1a70d7512ae10
Przemysław Trejnis <ptrejnis@gmail.com>
2022-12-20 20:04:58 +0100

    Feat: Add aoc 1a solution

--- src/day-1/a.ts
+++ src/day-1/a.ts
@@ -1,5 +1,10 @@
-const results = await Deno.readTextFile('./input.txt');
+const input = await Deno.readTextFile('./input.txt');
+const CHUNK_SEPARATOR = '\n\n';
+const NUMS_SEPARATOR = '\n';
+const sumKcals = (x) => x
+    .split(NUMS_SEPARATOR)
+    .reduce((curr, acc) => parseInt(curr, 10) + parseInt(acc, 10), 0);
+const portions = input.split(CHUNK_SEPARATOR).map(sumKcals);
+export const result = () => Math.max(...portions);
 
-export const result = () => console.log('res: ', results);
-
-result();
+console.log(result());
